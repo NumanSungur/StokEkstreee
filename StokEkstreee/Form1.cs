3b2e6e7eda0d8e7a40fd67af63061b416a172cb4
numan <numan@NUMANPC>
2022-01-23 21:46:04 +0300

    Add project files.

---
+++ StokEkstreee/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StokEkstreee
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=NUMANPC\MSSQLSERVER01;Initial Catalog=Test;Integrated Security=True");
+        SqlDataAdapter adapter;
+
+        DataTable table = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void listele()
+        {
+            adapter = new SqlDataAdapter("stokEkstre", con);
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+    }
+}
